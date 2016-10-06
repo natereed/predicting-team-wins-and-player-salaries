d6c2cf0e4610a5eb55e6f4a1f12876b513884803
Nate Reed <nate@natereed.com>
2016-10-06 10:37:27 -0500

    Scrape hitting and fielding for ALL players (add parameter). Update nameutils.py to handle suffixes. Update extract_player_urls.py to filter players that don't match the names in Salaries.csv. Will need to re-generate Salaries.csv with new names and run this on the new season data.

--- processing/extract_player_urls.py
+++ processing/extract_player_urls.py
@@ -1,15 +1,30 @@
 # Load player urls from data/cleaned
 # Use distinct player url's
+# Only use the intersection of players with Salaries (No point in downloading
+# stats for all players -- just the ones who have salaries).
 
 import csv
 import fnmatch
 import os
+import pandas as pd
+
+import nameutils
 from os import listdir
+
+# Function to convert player name to a player id (standardized short name)
+def player_id_from_name(name):
+    print(name)
+    return nameutils.normalize_last_and_first_initial(name)
+
+# Change this as needed:
+seasons = [2015]
+
+salaries_df = pd.read_csv(os.path.join("..", "data", "db", "Salaries.csv"))
+salaries_df = salaries_df[salaries_df['Year'].isin(seasons)]
 
 dir = os.path.join("..", "data", "cleaned")
 files = listdir(dir)
 
-seasons = ["2015"]
 player_urls = []
 players_output_dir = os.path.join("..", "data", "players")
 
@@ -19,7 +34,8 @@ for season in seasons:
             with open(os.path.join(dir, file), "r") as season_stats:
                 reader = csv.DictReader(season_stats)
                 for row in reader:
-                    player_urls.append(row['Player URL'])
+                    if player_id_from_name(row['Player']) in salaries_df['Player Id'].values:
+                        player_urls.append(row['Player URL'])
 
 print("{} url's".format(len(player_urls)))
 
--- processing/nameutils.py
+++ processing/nameutils.py
@@ -8,35 +8,52 @@ import re
 
 def normalize_first_and_last(name):
     name = name.strip()
-    name_and_suffix_pat = r'(.*),*\s+([J|S]r\.)$'
-    m = re.search(name_and_suffix_pat, name)
-
+    suffix = None
+    suffix_pat = r'([J|S]r\.{0,1})$'
+    m = re.search(suffix_pat, name)
     if m:
-        name = m.group(1).strip()
+        suffix = m.group(1)
+        # Remove suffix
+        name = re.sub(suffix_pat, '', name)
+        name = name.strip()
+
+    #name_and_suffix_pat = r'^([\w\s\.]+)(,{0,1}\s*[J|S]r\.{0,1})*$'
+    #m = re.search(name_and_suffix_pat, name)
+
+    #if m:
+    #    name = m.group(1).strip()
+    #    suffix = m.group(2)
+    #    if suffix:
+    #        suffix = suffix.strip()
 
     last = name.split()[-1]
     first = name.split()[0]
-    return normalized_name(first, last)
+    return normalized_name(first, last, suffix)
 
 def normalize_last_and_first_initial(name):
-    m = re.search(r'([\w\s]+),\s+([\w\s]{1})', name)
+    m = re.search(r'([\w\s]+)([J|S]r\.)*,\s+([\w\s]{1})', name)
     last = m.group(1)
-    first = m.group(2)
-    return normalized_name(first, last)
+    suffix = m.group(2)
+    first = m.group(3)
+    return normalized_name(first, last, suffix)
 
-def normalized_name(first, last):
+def normalized_name(first, last, suffix=None):
     norm_name = first[0].lower() + last.lower()
+    if (suffix):
+        norm_name = norm_name + suffix.lower()
     norm_name = norm_name.replace(' ', '')
+    norm_name = norm_name.replace('.', '')
     return norm_name
 
 name = 'Kevin J. Brown'
+print(name)
 norm_name = normalize_first_and_last(name)
 print("Normalized: " + norm_name)
 assert(norm_name == 'kbrown')
 
 norm_name = normalize_first_and_last('Ken Griffey Jr.')
 print("Normalized: " + norm_name)
-assert(norm_name == 'kgriffey')
+assert(norm_name == 'kgriffeyjr')
 
 norm_name = normalize_first_and_last('Chien-Ming Wang')
 print("Normalized: " + norm_name)
@@ -52,3 +69,7 @@ norm_name = normalize_last_and_first_initial(name)
 print("Normalized: " + norm_name)
 assert(norm_name == 'lwhitaker')
 
+name = "Young Jr., E"
+norm_name = normalize_last_and_first_initial(name)
+print("Normalized: " + norm_name)
+assert(norm_name == 'eyoungjr')
